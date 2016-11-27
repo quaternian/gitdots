504b9c6a165ddc7eed28e4dc0f40425dabdc58a8
quaternian <quaternian@users.noreply.github.com>
2016-11-27 15:00:00 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+## This should get everything up and running..
+
+git clone --bare https://github.com/quaternian/gitdots.git $HOME/.cfg
+function gitdots {
+   /usr/bin/git --git-dir=$HOME/.gitdots/ --work-tree=$HOME $@
+}
+mkdir -p .gitdots-backup
+gitdots checkout
+if [ $? = 0 ]; then
+  echo "Checked out gitdots.";
+  else
+    echo "Backing up pre-existing dot files.";
+    gitwrap checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+gitdots checkout
+gitdots config status.showUntrackedFiles no
